f66bc779b34805601b1889ca43dda29b69ff2e00
Mejell91 <150066198+Mejell91@users.noreply.github.com>
2024-02-14 20:34:00 -0500

    Add files via upload

---
+++ P1LAB1_RodgersJemell.py
@@ -0,0 +1,13 @@
+#Jemell Rodgers
+#2/14/2024
+#P1LAB1
+#Using input and output
+
+#Create a variable to hold first name
+first_name = input("Enter your first name: ")
+
+#Print first name with greeting
+print("Hey", first_name)
+#Put a blank line in between
+print()
+print("Welcome to CTI-110")
