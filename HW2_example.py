3c1750bfeadcb70512078ab58ba225769bd90473
Mejell91 <150066198+Mejell91@users.noreply.github.com>
2024-02-19 16:32:11 +0000

    First Upload

---
+++ HW2_example.py
@@ -0,0 +1,22 @@
+#Jemell Rodgers
+#2/19/24
+#Input and Output
+ 
+'''
+Pseudocode
+Get budget (int)
+Get destination (str)
+Get gas (int)
+Get hotel (int)
+Get food (int)
+Add expenses 
+budget - expenses
+Display results - use example
+
+'''
+
+print("This program calculates and displays travel expenses")
+#Get Budget fom user
+budget = int(input("Enter budget: ")
+dest= input("Enter destinaton: ")
+
